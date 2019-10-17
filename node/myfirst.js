29f382fde2986d3015716f899b24ea9a29e80dd2
Ikechukwu Uwah <iuwah@opay.team>
2019-10-17 17:42:18 +0100

    add upper-case npm package and mofify myfirst.js code to make use of it.

--- node/myfirst.js
+++ node/myfirst.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
+var uc = require('../node_modules/upper-case')
 
 //create a server object:
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'}); //write a response to the client
     res.write("The date and time are currently: " + dt.myDateTime()) //call the custom date module and write a response to the client
-    res.end('Hello World!'); //end the response
+    res.end(uc('Hello World!')); //end the response with the output in upper case
 }).listen(8080);//the server object listens on port 8080
